68c2de86fb8c8faa01d12b46e81352daa9372f76
RobinLarsson1 <robin.larsson01@gmail.com>
2023-05-15 11:41:01 +0200

    greeting added

--- start.js
+++ start.js
@@ -1,1 +1,8 @@
-console.log('hello world!')
+import { question } from "readline-sync"
+
+console.log('Welcome user!')
+let name = question('what is your name?: ')
+
+console.log(`Welcome again, ${name}!`)
+
+
